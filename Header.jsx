3d4dc3391144b2fd3f9c7ef562f61044475d09fd
Byamungu Rebecca <rebeccabyamungu6@gmail.com>
2025-04-15 23:39:54 -0700

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Header(){
+    return(
+        <div className="mt-8">
+        <span className="text-4xl font-bold text-gray-700 ">
+        My profile card</span>
+        
+        </div>
+    )
+}
