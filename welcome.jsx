67da16c48ae0f37997f24fd4334f9a71791395fa
Byamungu Rebecca <rebeccabyamungu6@gmail.com>
2025-04-15 23:40:32 -0700

    Add files via upload

---
+++ welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ export default  function Welcome(){
+    return(
+        <div>
+            <h1 className='text-black-400 font-bold'>Hello world !</h1>
+        </div>
+    )
+};
+
+
